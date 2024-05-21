2bd1eb9bb013e8accd399c027753ef53b74a05ac
thehuyvt <thehuyvtvp@gmail.com>
2024-05-21 17:56:18 +0700

    Update Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_number',
+        'address',
+        'gender',
+        'status',
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -18,6 +19,16 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
             'role' => 1,
+            'phone_number' => '0123456789',
+        ]);
+
+        Customer::create([
+            'name' => 'Huy',
+            'email' => 'huy@gmail.com',
+            'password' => Hash::make('123456'),
+            'address' => 'Ha Noi',
+            'gender' => 1,
+            'status' => 1,
             'phone_number' => '0123456789',
         ]);
     }
